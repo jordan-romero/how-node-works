7e365efcaad1f1257004df4fcd700b9d4dd5e6f5
jordles113 <jordan.t.romero@gmail.com>
2021-03-21 14:11:52 -0500

    learning how to use emitters:

--- event.js
+++ event.js
@@ -1,3 +1,15 @@
 const EventEmitter = require('events');
 
 const myEmitter = new EventEmitter();
+
+myEmitter.on('newSale', () => {
+  console.log('there was a new sale');
+});
+myEmitter.on('newSale', () => {
+  console.log('customer name: Jordan');
+});
+myEmitter.on('newSale', (stock) => {
+  console.log(`There are now ${stock} items left in stock`);
+});
+
+myEmitter.emit('newSale', 9);
