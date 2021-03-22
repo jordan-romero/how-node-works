4d61aae47208abf20bd2d0546dbf9b106ceb8e05
jordles113 <jordan.t.romero@gmail.com>
2021-03-22 11:27:14 -0500

    learning various ways to export info

--- modules.js
+++ modules.js
@@ -1,7 +1,11 @@
 const C = require('./test-module-1');
-
+const calc2 = require('./test-module-2');
 // console.log(arguments);
 // console.log(require('module').wrapper);
 
 const calc1 = new C();
 console.log(calc1.add(2, 5));
+
+//exports
+
+console.log(calc2.multiply(2, 5));
