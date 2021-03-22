4e50bcde6264646c225d8a3c42c8387ea5081dba
jordles113 <jordan.t.romero@gmail.com>
2021-03-22 11:24:55 -0500

    rewrites export module

--- modules.js
+++ modules.js
@@ -1,2 +1,7 @@
-console.log(arguments);
-console.log(require('module').wrapper);
+const C = require('./test-module-1');
+
+// console.log(arguments);
+// console.log(require('module').wrapper);
+
+const calc1 = new C();
+console.log(calc1.add(2, 5));
